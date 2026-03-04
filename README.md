12a4aec1b789e4d0cc2b26f82ce9c779521689c7
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-03-04 19:24:03 +0530

    Update Readme

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ Various algorithms and GFG, Leetcode problems are implemented in Java.
 
 ![img.png](img.png)
 
+Sheet: https://takeuforward.org/dsa/strivers-a2z-sheet-learn-dsa-a-to-z
 
-- Arrays
+- Bit Manipulation: [Reference](https://takeuforward.org/blogs/bit-manipulation?page=1)
+- Arrays [Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB)
 - String
 - Sliding Window : [Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL)
-- Bit Manipulation: [Reference](https://takeuforward.org/blogs/bit-manipulation?page=1)
+- Linked list [Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU)
+- Heap [Playlist](https://www.youtube.com/playlist?list=PL_z_8CaSLPWdtY9W22VjnPxG30CXNZpI9)
+- Stack & Queues [Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0pOd5zvVVSzgpo6BaCpHT9c)
+- Recursion [Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9)
--- src/main/java/stack/CelebrityProblem.java
+++ src/main/java/stack/CelebrityProblem.java
@@ -7,6 +7,28 @@ import java.util.Stack;
 public class CelebrityProblem {
 
 	static int celebrity(int M[][], int n){
+
+        // Brute force
+//        int n = mat.length;
+//
+//        int[] knowMe = new int[n];
+//        int[] iKnow = new int[n];
+//
+//        for(int i = 0; i < n; i++) {
+//            for(int j = 0; j < n; j++) {
+//                if(i != j && mat[i][j] == 1) {
+//                    knowMe[j]++;
+//                    iKnow[i]++;
+//                }
+//            }
+//        }
+//
+//        for(int i = 0; i < n; i++) {
+//            if(knowMe[i] == n-1 && iKnow[i] == 0) {
+//                return i;
+//            }
+//        }
+//        return -1;
         
         Stack<Integer>stack = new Stack<>();
         
@@ -35,6 +57,35 @@ public class CelebrityProblem {
         }
         
         return potential;
+
+//        int top = 0;
+//        int bottom = n - 1;
+//
+//        while(top < bottom) {
+//            if(mat[top][bottom] == 1) {
+//                top++;
+//            } else if (mat[bottom][top] == 1) {
+//                bottom--;
+//            } else {
+//                top++;
+//                bottom--;
+//            }
+//        }
+//
+//        if(top > bottom) {
+//            return -1;
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            if(i == top) {
+//                continue;
+//            }
+//            if(mat[top][i] != 0 || mat[i][top] !=1) {
+//                return -1;
+//            }
+//        }
+//
+//        return top;
     }
 	
 	public static void main(String[] args) {
