f585db799fa3b3085e1af9a936c9edb686e765dd
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-03-12 21:07:44 +0530

    Add subset problems

---
+++ src/main/java/backtracking/level1/SubsetSums.java
@@ -0,0 +1,27 @@
+package backtracking.level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SubsetSums {
+
+    public static void main(String[] args) {
+        int[] candidates = {1, 2, 1};
+
+        List<Integer> res = new ArrayList<>();
+        solve(candidates, 0, 0, res);
+        Collections.sort(res);
+        System.out.println(res);
+    }
+
+    private static void solve(int[] candidates, int index, int sum, List<Integer> res) {
+        if (index == candidates.length) {
+            res.add(sum);
+            return;
+        }
+
+        solve(candidates, index + 1, sum + candidates[index], res);
+        solve(candidates, index + 1, sum, res);
+    }
+}
