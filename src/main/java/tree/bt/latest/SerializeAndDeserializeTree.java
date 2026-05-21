86bbd1d8a8d7211a528672a571e55c4ab9697634
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-05-21 18:56:57 +0530

    Update SerializeAndDeserializeTree

--- src/main/java/tree/bt/latest/SerializeAndDeserializeTree.java
+++ src/main/java/tree/bt/latest/SerializeAndDeserializeTree.java
@@ -5,58 +5,54 @@ import java.util.Queue;
 
 public class SerializeAndDeserializeTree {
 
-    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null) {
-            return "";
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        if (root == null) return "";
 
         StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
         while (!queue.isEmpty()) {
             TreeNode node = queue.poll();
 
             if (node == null) {
-                sb.append("# ");
+                sb.append("#,");
                 continue;
             }
 
-            sb.append(node.data + " ");
-            queue.add(node.left);
-            queue.add(node.right);
+            sb.append(node.data).append(",");
+            queue.offer(node.left);
+            queue.offer(node.right);
         }
-
 
         return sb.toString();
     }
 
-    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data == null || data.isEmpty()) {
-            return null;
-        }
+        if (data == null || data.isEmpty()) return null;
 
-        String[] strArray = data.split(" ");
+        String[] arr = data.split(",");
 
+        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
         Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode root = new TreeNode(Integer.parseInt(strArray[0]));
-        queue.add(root);
+        queue.offer(root);
 
-        for (int i = 1; i < strArray.length; i++) {
+        int i = 1;
+
+        while (!queue.isEmpty() && i < arr.length) {
             TreeNode node = queue.poll();
 
-            if (!strArray[i].equals("#")) {
-                TreeNode left = new TreeNode(Integer.parseInt(strArray[i]));
-                queue.add(left);
-                node.left = left;
+            if (!arr[i].equals("#")) {
+                node.left = new TreeNode(Integer.parseInt(arr[i]));
+                queue.offer(node.left);
             }
-            if (!strArray[++i].equals("#")) {
-                TreeNode right = new TreeNode(Integer.parseInt(strArray[i]));
-                queue.add(right);
-                node.right = right;
+            i++;
+
+            if (i < arr.length && !arr[i].equals("#")) {
+                node.right = new TreeNode(Integer.parseInt(arr[i]));
+                queue.offer(node.right);
             }
+            i++;
         }
 
         return root;
