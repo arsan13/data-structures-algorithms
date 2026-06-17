b016e87ad3b87f100214be6ea300b414db2520f7
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-06-17 19:49:45 +0530

    Update ValidateTree

--- src/main/java/tree/bst/ValidateTree.java
+++ src/main/java/tree/bst/ValidateTree.java
@@ -16,4 +16,21 @@ public class ValidateTree {
 
 		return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
 	}
+
+	public boolean isValidBST(Node root) {
+		return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+	}
+
+	private boolean isValidBST(Node root, long min, long max) {
+		if (root == null) {
+			return true;
+		}
+
+		if (root.data <= min || root.data >= max) {
+			return false;
+		}
+
+		return isValidBST(root.left, min, root.data) &&
+				isValidBST(root.right, root.data, max);
+	}
 }
