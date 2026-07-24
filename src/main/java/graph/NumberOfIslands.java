8a07edf0fe8c7d314023a306408a382e38536bdb
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-07-24 11:42:35 +0530

    Update Graph NumberOfIslands problem

--- src/main/java/graph/NumberOfIslands.java
+++ src/main/java/graph/NumberOfIslands.java
@@ -18,7 +18,6 @@ public class NumberOfIslands {
         
         return count;
     }
-    
     private static void traverse(int[][] grid, int i, int j) {
         
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length)
@@ -32,6 +31,40 @@ public class NumberOfIslands {
         traverse(grid, i-1, j);
         traverse(grid, i, j-1);
         traverse(grid, i, j+1);
+    }
+
+    // Without mutating given grid
+    public int numIslands(char[][] grid) {
+
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(i, j, grid, visited);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int row, int col, char[][] grid, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+            return;
+        }
+        if (grid[row][col] == '0' || visited[row][col]) {
+            return;
+        }
+
+        visited[row][col] = true;
+
+        dfs(row - 1, col, grid, visited);
+        dfs(row, col + 1, grid, visited);
+        dfs(row + 1, col, grid, visited);
+        dfs(row, col - 1, grid, visited);
     }
 	
 	public static void main(String[] args) {
