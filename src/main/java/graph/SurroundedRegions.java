dc1081f0e42712b1002d9336a9a2e1ced316e7b2
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-07-30 19:24:16 +0530

    Added SurroundedRegions

---
+++ src/main/java/graph/SurroundedRegions.java
@@ -0,0 +1,83 @@
+package graph;
+
+/*
+You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+Connect: A cell is connected to adjacent cells horizontally or vertically.
+Region: To form a region connect every 'O' cell.
+Surround: A region is surrounded if none of the 'O' cells in that region are on the edge of the board. Such regions are completely enclosed by 'X' cells.
+To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+ */
+public class SurroundedRegions {
+
+    private static final int[] dx = {-1, 0, 1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
+
+    // 0s connected with the 0s present in boundary cannot be converted to X.
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+
+        // First and Last row
+        for (int j = 0; j < m; j++) {
+            if (!visited[0][j] && board[0][j] == 'O') {
+                dfs(0, j, board, visited);
+            }
+            if (!visited[n - 1][j] && board[n - 1][j] == 'O') {
+                dfs(n - 1, j, board, visited);
+            }
+        }
+
+        // First and Last Columns
+        for (int i = 0; i < n; i++) {
+            if (!visited[i][0] && board[i][0] == 'O') {
+                dfs(i, 0, board, visited);
+            }
+            if (!visited[i][m - 1] && board[i][m - 1] == 'O') {
+                dfs(i, m - 1, board, visited);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void dfs(int row, int col, char[][] board, boolean[][] visited) {
+        int n = board.length;
+        int m = board[0].length;
+
+        visited[row][col] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && board[newRow][newCol] == 'O' && !visited[newRow][newCol]) {
+                dfs(newRow, newCol, board, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][] {
+                {'X','X','X','X'},
+                {'X','O','O','X'},
+                {'X','X','O','X'},
+                {'X','O','X','X'}
+        };
+
+        new SurroundedRegions().solve(board);
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
