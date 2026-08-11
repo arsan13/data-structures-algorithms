257be80c15514a35123306beb69888818d00f811
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-08-11 13:00:02 +0530

    Added Topo sort problems

--- src/main/java/graph/topologicalSort/TopologicalSortUsingBFS.java
+++ src/main/java/graph/topologicalSort/TopologicalSortUsingBFS.java
@@ -2,57 +2,65 @@ package graph.topologicalSort;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class TopologicalSortUsingBFS {
-	
-	public static void topologicalSort(ArrayList<ArrayList<Integer>> adj, int n) {
-		
-		Queue<Integer> queue = new LinkedList<>();
-		int inDegree[] = new int[n];
-		
-		// Find in-degree
-		for(int i = 0; i < n; i++) {
-			for(int v : adj.get(i)) {
-				inDegree[v]++;
-			}
-		}
-		
-		// Push 0 degree nodes to queue
-		for(int i = 0; i < n; i++) {
-			if(inDegree[i] == 0)
-				queue.add(i);
-		}
-		
-		while(!queue.isEmpty()) {
-			int u = queue.poll();
-			System.out.println(u);
-			for(Integer v : adj.get(u)) {
-				inDegree[v]--;
-				if(inDegree[v] == 0)
-					queue.add(v);
-			}
-		}
-	}
 
-	public static void main(String[] args) {
-		
-		int n = 7;
-		
-		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-		
-		for(int i = 0; i < n; i++)
-			adj.add(new ArrayList<Integer>());
-		
-		adj.get(0).add(1);
-		adj.get(0).add(3);
-		adj.get(1).add(2);
-		adj.get(2).add(3);
-		adj.get(4).add(3);
-		adj.get(4).add(5);
-		adj.get(4).add(6);
-		
-		topologicalSort(adj, n);
-	}
+    public static List<Integer> topologicalSort(List<ArrayList<Integer>> adj, int v) {
+
+        // 1. Calculate indegree
+        int[] indegree = new int[v];
+        for (int i = 0; i < v; i++) {
+            for (int j : adj.get(i)) {
+                indegree[j]++;
+            }
+        }
+
+        // 2. Insert zero indegree nodes to queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < v; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        // 3. Update indegree, push to queue if zero indegree and populate result
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+
+            for (int i : adj.get(node)) {
+                indegree[i]--;
+                if (indegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<Integer>());
+
+        adj.get(0).add(1);
+        adj.get(0).add(3);
+        adj.get(1).add(2);
+        adj.get(2).add(3);
+        adj.get(4).add(3);
+        adj.get(4).add(5);
+        adj.get(4).add(6);
+
+        final List<Integer> list = topologicalSort(adj, n);
+        System.out.println(list);
+    }
 
 }
--- src/main/java/graph/topologicalSort/TopologicalSortUsingDFS.java
+++ src/main/java/graph/topologicalSort/TopologicalSortUsingDFS.java
@@ -1,55 +1,59 @@
 package graph.topologicalSort;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class TopologicalSortUsingDFS {
-	
-	private static void topologicalSortUtil(int src, ArrayList<ArrayList<Integer>> adj, boolean[] visited,
-			Stack<Integer> stack) {
-		
-		visited[src] = true;
-		
-		for(int neighbour : adj.get(src)) {
-			if(!visited[neighbour])
-			topologicalSortUtil(neighbour, adj, visited, stack);
-		}
-		
-		stack.push(src);
-	}
-	
-	public static void topologicalSort(ArrayList<ArrayList<Integer>> adj, int n) {
-		
-		boolean[] visited = new boolean[n];
-		Stack<Integer> stack = new Stack<>();
-		
-		for(int i = 0; i < n; i++) {
-			if(!visited[i])
-				topologicalSortUtil(i, adj, visited, stack);
-		}
-		
-		while(!stack.isEmpty())
-			System.out.println(stack.pop());
-	}
 
-	public static void main(String[] args) {
-		
-		int n = 7;
-		
-		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-		
-		for(int i = 0; i < n; i++)
-			adj.add(new ArrayList<Integer>());
-		
-		adj.get(0).add(1);
-		adj.get(0).add(3);
-		adj.get(1).add(2);
-		adj.get(2).add(3);
-		adj.get(4).add(3);
-		adj.get(4).add(5);
-		adj.get(4).add(6);
-		
-		topologicalSort(adj, n);
-	}
+    private static void dfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> stack) {
+        visited[src] = true;
+
+        for (int neighbour : adj.get(src)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, adj, visited, stack);
+            }
+        }
+
+        stack.push(src);
+    }
+
+    public static List<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj, int n) {
+        boolean[] visited = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, stack);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(0).add(1);
+        adj.get(0).add(3);
+        adj.get(1).add(2);
+        adj.get(2).add(3);
+        adj.get(4).add(3);
+        adj.get(4).add(5);
+        adj.get(4).add(6);
+
+        final List<Integer> list = topologicalSort(adj, n);
+        System.out.println(list);
+    }
 
 }
